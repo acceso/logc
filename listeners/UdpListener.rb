d96cf498e38f12eb0c04d9758924d02475116e02
José Sánchez <jose@ssanchez.net>
2012-08-11 15:34:46 +0200

    Add udp listener (untested but it should work).

---
+++ listeners/UdpListener.rb
@@ -0,0 +1,36 @@
+# vim: sw=2 sts=2 et ts=2
+
+
+
+class UdpListenerCB < EM::Connection
+
+  def receive_data( data )
+    msg = Msg.new
+
+    msg.tstamp = Time.now.getutc.to_f
+    msg.channel = @channel
+    msg.body = data
+
+    @routerhandler.route( @channel, msg )
+  end
+
+
+  def initialize( routerhandler, channel )
+    @routerhandler = routerhandler
+    @channel = channel
+  end
+
+end
+
+
+
+class UdpListener
+
+  def initialize( routerhandler, channel, ip, port )
+    EM::open_datagram_socket( ip, port, UdpListenerCB, routerhandler, channel )
+  end
+
+end
+
+
+
