53d745dbcb4be314f1a96e12ae5968de9c999973
José Sánchez <jose@ssanchez.net>
2012-08-12 20:23:34 +0200

    Add tcp sender.

---
+++ handlers/TcpSender.rb
@@ -0,0 +1,40 @@
+# vim: sw=2 sts=2 et ts=2
+
+
+
+class TcpSenderH < EM::Connection
+
+  def receive_data( data )
+    msg = Msg.new
+
+    msg.tstamp = Time.now.getutc.to_f
+    msg.channel = @channel
+    msg.body = data
+
+    @routerhandler.route( @channel, msg )
+  end
+
+  def send( msg )
+    #p msg.channel + " routing msg: " + msg.body
+
+    self.send_data( msg.body )
+  end
+
+end
+
+
+
+class TcpSender
+
+  def initialize( routerhandler, channel, ip, port, timeout = nil )
+    @channel = channel
+
+    c = EM::connect( ip, port, TcpSenderH )
+
+    routerhandler.route_add( channel, c )
+  end
+
+end
+
+
+
